b5f857ca6fd3aa7e32d027ed8cb3c23b9ad33944
jbettenh <21023218+jbettenh@users.noreply.github.com>
2020-11-04 23:08:00 +0100

    most prolific automaker

--- pybites_data_formats.py
+++ pybites_data_formats.py
@@ -14,10 +14,22 @@ with requests.Session() as s:
 def most_prolific_automaker(year):
     """Given year 'year' return the automaker that released
        the highest number of new car models"""
-    pass
+    automaker = {}
+    for manf in data:
+        automaker[manf['automaker']] = 0
 
+    for model in data:
+        if model['year'] == year:
+            automaker[model['automaker']] = automaker.get(model['automaker']) + 1
+
+    most_new_cars = max(automaker, key=automaker.get)
+
+    return most_new_cars
 
 def get_models(automaker, year):
     """Filter cars 'data' by 'automaker' and 'year',
        return a set of models (a 'set' to avoid duplicate models)"""
     pass
+
+
+print(most_prolific_automaker(1999))
